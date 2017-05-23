3f738b4e32b2e67485ed6741790378b07948aa37
Geraint Edwards <github@copyn.plus.com>
2017-05-23 14:11:56 +0100

    Missing closing div

--- index.php
+++ index.php
@@ -6,12 +6,10 @@
  * @copyright   A copyright
  * @license     A "Slug" license name e.g. GPL2
  */
-
 defined('_JEXEC') or die;
 
 /** @var JDocumentHtml $this */
-
-$app  = JFactory::getApplication();
+$app = JFactory::getApplication();
 $user = JFactory::getUser();
 
 // Output as HTML5
@@ -21,11 +19,11 @@ $this->setHtml5(true);
 $params = $app->getTemplate(true)->params;
 
 // Detecting Active Variables
-$option   = $app->input->getCmd('option', '');
-$view     = $app->input->getCmd('view', '');
-$layout   = $app->input->getCmd('layout', '');
-$task     = $app->input->getCmd('task', '');
-$itemid   = $app->input->getCmd('Itemid', '');
+$option = $app->input->getCmd('option', '');
+$view = $app->input->getCmd('view', '');
+$layout = $app->input->getCmd('layout', '');
+$task = $app->input->getCmd('task', '');
+$itemid = $app->input->getCmd('Itemid', '');
 $sitename = $app->get('sitename');
 
 //Load in the Style Sheets
@@ -33,42 +31,38 @@ JHtml::_('stylesheet', 'uikit.css', array('version' => 'auto', 'relative' => tru
 
 //Added for if we want offline.css file specific to the offline page.
 //JHtml::_('stylesheet', 'offline.css', array('version' => 'auto', 'relative' => true));
-
 //Load in JavaScript
 JHtml::_('jquery.framework');
 JHtml::_('script', 'uikit.js', array('version' => 'auto', 'relative' => true));
-
 ?>
 
 <html lang="en-gb" dir="ltr" class="uk-notouch"><head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<jdoc:include type="head" />
+    <jdoc:include type="head" />
 </head>
 <body>
 
-<div class="uk-container uk-container-center uk-margin-top uk-margin-large-bottom <?php echo $option
-	. ' view-' . $view
-	. ($layout ? ' layout-' . $layout : ' no-layout')
-	. ($task ? ' task-' . $task : ' no-task')
-	. ($itemid ? ' itemid-' . $itemid : '')
-	. ($params->get('fluidContainer') ? ' fluid' : '');
-echo ($this->direction === 'rtl' ? ' rtl' : '');
-?>">
-
+    <div class="uk-container uk-container-center uk-margin-top uk-margin-large-bottom <?php
+    echo $option
+    . ' view-' . $view
+    . ($layout ? ' layout-' . $layout : ' no-layout')
+    . ($task ? ' task-' . $task : ' no-task')
+    . ($itemid ? ' itemid-' . $itemid : '')
+    . ($params->get('fluidContainer') ? ' fluid' : '');
+    echo ($this->direction === 'rtl' ? ' rtl' : '');
+    ?>">
 	<div class="uk-grid" data-uk-grid-margin="">
-		<div class="uk-width-medium-1-1 uk-row-first">
+	    <div class="uk-width-medium-1-1 uk-row-first">
 
-			<div class="uk-vertical-align uk-text-center" style="background: url('data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4NCjwhLS0gR2VuZXJhdG9yOiBBZG9iZSBJbGx1c3RyYXRvciAxNi4wLjQsIFNWRyBFeHBvcnQgUGx1Zy1JbiAuIFNWRyBWZXJzaW9uOiA2LjAwIEJ1aWxkIDApICAtLT4NCjwhRE9DVFlQRSBzdmcgUFVCTElDICItLy9XM0MvL0RURCBTVkcgMS4xLy9FTiIgImh0dHA6Ly93d3cudzMub3JnL0dyYXBoaWNzL1NWRy8xLjEvRFREL3N2ZzExLmR0ZCI+DQo8c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkViZW5lXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4Ig0KCSB3aWR0aD0iMTEzMHB4IiBoZWlnaHQ9IjQ1MHB4IiB2aWV3Qm94PSIwIDAgMTEzMCA0NTAiIGVuYWJsZS1iYWNrZ3JvdW5kPSJuZXcgMCAwIDExMzAgNDUwIiB4bWw6c3BhY2U9InByZXNlcnZlIj4NCjxyZWN0IGZpbGw9IiNGNUY1RjUiIHdpZHRoPSIxMTMwIiBoZWlnaHQ9IjQ1MCIvPg0KPC9zdmc+DQo=') 50% 0 no-repeat; height: 450px;">
-				<div class="uk-vertical-align-middle uk-width-1-2">
-					<jdoc:include type="message" />
-					<jdoc:include type="component" />
-				</div>
-			</div>
+		<div class="uk-vertical-align uk-text-center" style="background: url('data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4NCjwhLS0gR2VuZXJhdG9yOiBBZG9iZSBJbGx1c3RyYXRvciAxNi4wLjQsIFNWRyBFeHBvcnQgUGx1Zy1JbiAuIFNWRyBWZXJzaW9uOiA2LjAwIEJ1aWxkIDApICAtLT4NCjwhRE9DVFlQRSBzdmcgUFVCTElDICItLy9XM0MvL0RURCBTVkcgMS4xLy9FTiIgImh0dHA6Ly93d3cudzMub3JnL0dyYXBoaWNzL1NWRy8xLjEvRFREL3N2ZzExLmR0ZCI+DQo8c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkViZW5lXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4Ig0KCSB3aWR0aD0iMTEzMHB4IiBoZWlnaHQ9IjQ1MHB4IiB2aWV3Qm94PSIwIDAgMTEzMCA0NTAiIGVuYWJsZS1iYWNrZ3JvdW5kPSJuZXcgMCAwIDExMzAgNDUwIiB4bWw6c3BhY2U9InByZXNlcnZlIj4NCjxyZWN0IGZpbGw9IiNGNUY1RjUiIHdpZHRoPSIxMTMwIiBoZWlnaHQ9IjQ1MCIvPg0KPC9zdmc+DQo=') 50% 0 no-repeat; height: 450px;">
+		    <div class="uk-vertical-align-middle uk-width-1-2">
+			<jdoc:include type="message" />
+			<jdoc:include type="component" />
+		    </div>
+		</div>
+	    </div> 
 	</div>
-
-</div>
-
-
+    </div>
 </body>
 </html>
